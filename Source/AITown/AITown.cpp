5b163ae72fd889cf711ef87ab136333439475b94
Camilo Espinosa <camesp96@gmail.com>
2023-01-21 21:37:03 -0700

    Initial commit

---
+++ Source/AITown/AITown.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AITown.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AITown, "AITown" );
---
+++ Source/AITown/AITownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AITownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AITOWN_API AAITownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
